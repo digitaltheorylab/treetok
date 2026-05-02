4385336f1230880194b2a97d530b09e0fbcd005d
Tyler Shoemaker <69219954+t-shoemaker@users.noreply.github.com>
2026-05-02 12:40:25 -0500

    Update shipped model dataset/training params

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env zsh
 
 set -euo pipefail
+trap 'echo -e "\nInterrupted. Exiting..."; exit 130' INT
+
 OUTDIR="data/"
 CLF="model.json"
 
@@ -13,8 +15,10 @@ typeset -A MODELS=(
 )
 
 for model filename in "${(@kv)MODELS}"; do
-  python -m treetok build-dataset "$model" -o "${OUTDIR}${filename}"
+  python -m treetok build-dataset "$model" -o "${OUTDIR}${filename}" \
+    --n-positives 2500 --n-hard-negatives 10000 --n-easy-negatives 2500
 done
 
-python -m treetok train ${OUTDIR}*.parquet -o "${OUTDIR}${CLF}"
+python -m treetok train ${OUTDIR}*.parquet -o "${OUTDIR}${CLF}" \
+  --val-size 0.5 --merge-threshold-floor 0.92
 
